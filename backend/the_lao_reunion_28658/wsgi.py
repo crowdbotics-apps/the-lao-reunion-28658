7879e77b2383582cd0e007cd94261bd75d3a6686
Crowdbotics <team@crowdbotics.com>
2021-07-09 05:52:21 +0000

    Update models

--- backend/the_lao_reunion_28658/wsgi.py
+++ backend/the_lao_reunion_28658/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_lao_reunion_28658.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the_lao_reunion_28658.settings")
 
 application = get_wsgi_application()
